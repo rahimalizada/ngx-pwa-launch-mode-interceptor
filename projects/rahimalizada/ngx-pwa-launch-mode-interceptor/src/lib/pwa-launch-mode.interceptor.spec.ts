ed00aa47e7e9a74b1aedc91fc50f6d8c37ec0316
Rahim Alizada <rahim.alizada@gmail.com>
2020-05-01 16:22:41 +0400

    Added a test

--- projects/rahimalizada/ngx-pwa-launch-mode-interceptor/src/lib/pwa-launch-mode.interceptor.spec.ts
+++ projects/rahimalizada/ngx-pwa-launch-mode-interceptor/src/lib/pwa-launch-mode.interceptor.spec.ts
@@ -1,16 +1,40 @@
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { PwaLaunchModeInterceptor } from './pwa-launch-mode.interceptor';
 
 describe('PwaLaunchModeInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      PwaLaunchModeInterceptor
-      ]
-  }));
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
 
-  it('should be created', () => {
-    const interceptor: PwaLaunchModeInterceptor = TestBed.inject(PwaLaunchModeInterceptor);
-    expect(interceptor).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: PwaLaunchModeInterceptor,
+          multi: true,
+        },
+      ],
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+  });
+
+  it('should add a header', () => {
+    httpClient.get('http://google.com').subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne('http://google.com');
+    expect(httpRequest.request.headers.has('X-PWA-Launch-Mode')).toEqual(true);
+    expect(httpRequest.request.headers.get('X-PWA-Launch-Mode')).toEqual(
+      'browser'
+    );
   });
 });
